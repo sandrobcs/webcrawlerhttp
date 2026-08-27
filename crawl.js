52575f6d5ee5094ca7e15009d2a6d14d32c14a3b
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-08-27 17:40:34 -0300

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,45 +1,69 @@
-const { JSDOM } = require(`jsdom`)
+const { JSDOM } = require(`jsdom`);
 
-function normalizeURL(urlString) {
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1)
+async function crawlPage(currentURL) {
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code: ${resp.status} on page: ${currentURL}`,
+      );
+      return;
     }
-    return hostPath
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non html response, content type: ${contentType} on page: ${currentURL}`,
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+    
+  } catch (err) {
+    console.log(`error in fetch: ${err.message} in page: ${currentURL}`);
+  }
 }
 
-function getURLsFromHTML(HTMLBodyElement, baseURL){
-    const urls = []
-    const dom = new JSDOM(HTMLBodyElement)
-    const linkElements = dom.window.document.querySelectorAll('a')
-    
-    for (const linkElement of linkElements) {
-        if(linkElement.href.slice(0, 1) === '/'){
-            //relative url
-            try {
-                const urlObj = new URL(`${baseURL}${linkElement.href}`)
-                urls.push(urlObj.href)
-            } catch (err) {
-                console.log(`erro with relative url: ${err.message}`)
-            }
-            
-        } else {
-            //absolute url
-            try {
-                const urlObj = new URL(linkElement.href)
-                urls.push(urlObj.href)
-            } catch (err) {
-                console.log(`erro with absolute url: ${err.message}`)
-            }
-            
-        }
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+
+function getURLsFromHTML(HTMLBodyElement, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(HTMLBodyElement);
+  const linkElements = dom.window.document.querySelectorAll("a");
+
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative url
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`erro with relative url: ${err.message}`);
+      }
+    } else {
+      //absolute url
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`erro with absolute url: ${err.message}`);
+      }
     }
-    return urls
+  }
+  return urls;
 }
 
 module.exports = {
-    normalizeURL,
-    getURLsFromHTML
-}
+  normalizeURL,
+  getURLsFromHTML,
+  crawlPage,
+};
